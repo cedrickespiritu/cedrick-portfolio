db61103e2b3d82dec15a694b433738af51a6099f
Cedrick Espiritu <2020919@ub.edu.ph>
2024-09-07 14:05:46 +0800

    project modal fix

--- src/components/ProjectModal.jsx
+++ src/components/ProjectModal.jsx
@@ -4,33 +4,33 @@ import './project-modal.css';
 const projectData = {
   'Web Development': [
     {
-      image: 'src/assets/img/project-img/web-development/ub-360.png',
+      image: '/assets/img/project-img/web-development/ub-360.png',
       description: 'UB 360',
       demoLink: 'https://ublc-360.vercel.app',
       codeLink: 'https://github.com/cedrickespiritu/nadafa-main',
     },
     {
-      image: 'src/assets/img/project-img/web-development/fineline-design.png',
+      image: '/assets/img/project-img/web-development/fineline-design.png',
       description: 'Fineline Design',
       demoLink: 'https://finelinedesign.vercel.app',
       codeLink: 'https://github.com/cedrickespiritu/FineLineDesign',
     },
     {
-      image: 'src/assets/img/project-img/web-development/taylorsweep.png',
+      image: '/assets/img/project-img/web-development/taylorsweep.png',
       description: 'Taylor Sweep',
       demoLink: 'https://nadafa-main.vercel.app/',
       codeLink: 'https://github.com/cedrickespiritu/nadafa-main',
     },
     {
-      image: 'src/assets/img/project-img/web-development/ub-website.png',
+      image: '/assets/img/project-img/web-development/ub-website.png',
       description: 'University Of Batangas Website Redesign',
     },
     {
-      image: 'src/assets/img/project-img/web-development/portfolio-2.png',
+      image: '/assets/img/project-img/web-development/portfolio-2.png',
       description: 'Portfolio Website Design',
     },
     {
-      image: 'src/assets/img/project-img/web-development/bigben.jpg',
+      image: '/assets/img/project-img/web-development/bigben.jpg',
       description: 'Design for Online Food App',
     },
     
@@ -39,112 +39,112 @@ const projectData = {
   ],
   'Motion Graphics': [
     {
-      image: 'src/assets/img/project-img/motion-graphics/cbi-intro.png',
+      image: '/assets/img/project-img/motion-graphics/cbi-intro.png',
       description: 'Center for Business and Innovation Intro',
       demoLink: 'https://drive.google.com/file/d/17MPu2_UjbOj4VZrjs6RryN4FX9ICNXMh/view?usp=sharing',
     },
   ],
   'Print Materials': [
     {
-      image: 'src/assets/img/project-img/print-materials/standee.png',
+      image: '/assets/img/project-img/print-materials/standee.png',
       description: 'Standee',
     },
     {
-      image: 'src/assets/img/project-img/print-materials/newsletter.jpg',
+      image: '/assets/img/project-img/print-materials/newsletter.jpg',
       description: 'Newsletter',
     },
     {
-      image: 'src/assets/img/project-img/print-materials/packaging.png',
+      image: '/assets/img/project-img/print-materials/packaging.png',
       description: 'Packaging',
     },
     {
-      image: 'src/assets/img/project-img/print-materials/tickets.png',
+      image: '/assets/img/project-img/print-materials/tickets.png',
       description: 'Tickets',
     },
     {
-      image: 'src/assets/img/project-img/print-materials/menu.png',
+      image: '/assets/img/project-img/print-materials/menu.png',
       description: 'Menu',
     },
     {
-      image: 'src/assets/img/project-img/print-materials/business-card.png',
+      image: '/assets/img/project-img/print-materials/business-card.png',
       description: 'Business Card',
     },
   ],
   'Animation': [
     {
-      image: 'src/assets/img/project-img/animation/nagkabangaan.png',
+      image: '/assets/img/project-img/animation/nagkabangaan.png',
       description: 'Meme Animation',
       demoLink: 'https://drive.google.com/file/d/1py5RKdxjhAr0dCgLDgwTeZYAOUMh9eMs/view?usp=sharing',
     },
     {
-      image: 'src/assets/img/project-img/animation/lipsync.png',
+      image: '/assets/img/project-img/animation/lipsync.png',
       description: 'Lip Syncing',
       demoLink: 'https://drive.google.com/file/d/1FIZELfKLlftJeP_IhvRfcCtgEPbo3esm/view?usp=sharing',
     },
   ],
   'Vector/Vexel': [
     {
-      image: 'src/assets/img/project-img/vexel/buena.png',
+      image: '/assets/img/project-img/vexel/buena.png',
     },
     {
-      image: 'src/assets/img/project-img/vexel/apollo.png',
+      image: '/assets/img/project-img/vexel/apollo.png',
     },
     {
-      image: 'src/assets/img/project-img/vexel/athena.png',
+      image: '/assets/img/project-img/vexel/athena.png',
     },
     {
-      image: 'src/assets/img/project-img/vexel/cyberpunk.png',
+      image: '/assets/img/project-img/vexel/cyberpunk.png',
     },
   ],
   'Shirt Designs': [
     {
-      image: 'src/assets/img/project-img/shirt-design/shirt1.png',
+      image: '/assets/img/project-img/shirt-design/shirt1.png',
     },
     {
-      image: 'src/assets/img/project-img/shirt-design/shirt2.png',
+      image: '/assets/img/project-img/shirt-design/shirt2.png',
     },
     {
-      image: 'src/assets/img/project-img/shirt-design/shirt3.png',
+      image: '/assets/img/project-img/shirt-design/shirt3.png',
     },
   ],
   'Logo Design': [
     {
-      image: 'src/assets/img/project-img/logo/logo1.png',
+      image: '/assets/img/project-img/logo/logo1.png',
     },
     {
-      image: 'src/assets/img/project-img/logo/logo2.png',
+      image: '/assets/img/project-img/logo/logo2.png',
     },
     {
-      image: 'src/assets/img/project-img/logo/logo3.png',
+      image: '/assets/img/project-img/logo/logo3.png',
     },
     {
-      image: 'src/assets/img/project-img/logo/logo4.png',
+      image: '/assets/img/project-img/logo/logo4.png',
     },
   ],
   'SocMed Poster': [
     {
-      image: 'src/assets/img/project-img/poster/poster1.png',
+      image: '/assets/img/project-img/poster/poster1.png',
     },
     {
-      image: 'src/assets/img/project-img/poster/poster2.png',
+      image: '/assets/img/project-img/poster/poster2.png',
     },
     {
-      image: 'src/assets/img/project-img/poster/poster3.jpg',
+      image: '/assets/img/project-img/poster/poster3.jpg',
     },
     {
-      image: 'src/assets/img/project-img/poster/poster4.png',
+      image: '/assets/img/project-img/poster/poster4.png',
     },
     {
-      image: 'src/assets/img/project-img/poster/poster5.png',
+      image: '/assets/img/project-img/poster/poster5.png',
     },
     {
-      image: 'src/assets/img/project-img/poster/poster6.png',
+      image: '/assets/img/project-img/poster/poster6.png',
     },
     {
-      image: 'src/assets/img/project-img/poster/poster7.png',
+      image: '/assets/img/project-img/poster/poster7.png',
     },
     {
-      image: 'src/assets/img/project-img/poster/poster8.png',
+      image: '/assets/img/project-img/poster/poster8.png',
     },
   ],
 };
@@ -153,7 +153,7 @@ const ProjectModal = ({ handleClose, projectType }) => {
   const projects = projectData[projectType];
 
   return (
-    <div className="fixed inset-0 flex items-center justify-center bg-gray-900 bg-opacity-50 text-primary">
+    <div className="fixed inset-0 flex items-center justify-center bg-gray-900 bg-opacity-50 text-primary top-[2rem]">
       <div className="bg-white w-[90%] max-w-[1000px] max-h-[80%] overflow-y-auto p-6 rounded-lg shadow-lg relative sm:min-w-[60vw]">
         <h2 className="text-xl font-bold mb-4">{projectType}</h2>
 
