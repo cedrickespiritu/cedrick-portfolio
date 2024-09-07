19c0f26c6ab7774ce3e1523f1a5eeb58bca2af58
Cedrick Espiritu <2020919@ub.edu.ph>
2024-09-07 13:44:00 +0800

    update hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,9 +8,9 @@ const Hero = () => {
   };
 
   return (
-    <div className="h-screen bg-cover bg-center bg-no-repeat" style={{ backgroundImage: `url(../assets/img/hero-bg.png)`, backgroundColor: '#20202b',backgroundBlendMode: 'overlay', }}>
+    <div className="h-screen bg-cover bg-center bg-no-repeat" style={{ backgroundImage: `url(/assets/img/hero-bg.png)`, backgroundColor: '#20202b',backgroundBlendMode: 'overlay', }}>
       <div class="text-container items-center lg:items-start max-w-[1000px] mx-auto px-8 flex flex-col justify-center min-h-[85vh]">
-        <span class="text-[4rem] lg:text-[5rem] text-white font-semibold text-center">I'm  <span className="text-secondary text-[4rem] lg:text-[5rem]">{name}</span></span>
+        <span class="text-[4rem] lg:text-[5rem] text-white font-semibold text-center">Iasd'm  <span className="text-secondary text-[4rem] lg:text-[5rem]">{name}</span></span>
         <span class="text-3xl md:text-4xl text-center lg:text-start lg:text-6xl font-medium text-white mb-7">A Multimedia Designer <br /> and Web Developer</span>
         <span className="specialized">
           <ul className='text-primary flex flex-row justify-center flex-wrap font-[600] text-[15px] lg:text-[20px] gap-[20px] mb-8 cursor-default'>
