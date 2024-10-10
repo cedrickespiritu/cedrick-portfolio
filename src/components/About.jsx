f6068eda2ba3df1171ba7e3e6aa8548f5b5e543e
cedrickespiritu <90923694+cedrickespiritu@users.noreply.github.com>
2024-10-10 21:50:37 +0800

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -102,8 +102,8 @@ const About = () => {
                     <li className='3d-rendering flex gap-[.5rem] flex-wrap'>
                       <span className="web-designing">Web Designing -</span>
                       <span className="icon-container flex h-fit gap-[.5rem]">
-                        <img src="/assets/img/skills-icon/figma.png" className="w-[40px] h-fit" alt="" />
-                        <img src="/assets/img/skills-icon/framer.png" className="w-[40px] h-fit" alt="" />
+                        <img src="/assets/img/skills-icon/figma.png" className="w-[30px] h-fit" alt="" />
+                        <img src="/assets/img/skills-icon/framer.png" className="w-[50px] h-fit" alt="" />
                       </span>
                     </li>
                     <li className='frontend-stack flex gap-[.5rem] flex-wrap'>
